7749e34f25e36925a2ef895df1061bf1429ff345
mgm-lt133 <a8122135@gsuite.si.aoyama.ac.jp>
2025-11-15 18:24:02 +0900

    CSS修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    // './styles/globals.css', // 👈 この行を追加
+    // './styles/globals.css', // 👈 この行を削除またはコメントアウトします
   ],
   theme: {
     extend: {},
