8461bde277f06ce22d6289c23071cb79688d9a44
mgm-lt133 <a8122135@gsuite.si.aoyama.ac.jp>
2025-11-15 17:55:13 +0900

    tailwind削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './styles/globals.css', // 👈 この行を追加
+    // './styles/globals.css', // 👈 この行を追加
   ],
   theme: {
     extend: {},
